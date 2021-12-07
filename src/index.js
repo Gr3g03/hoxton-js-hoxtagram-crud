7e24041ae2b7985b04dce90c715c4933dd2337f7
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2021-12-07 14:25:11 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,86 @@
+const imageContainer = document.querySelector('.image-container')
+const cardImgEl = document.querySelector('.image-card')
+
+
+const state = {
+    images: []
+}
+
+function getImagesFromServer(){
+   return fetch("http://localhost:3000/images")
+   .then(function(resp){
+     return resp.json()
+    })
+    // .then(function(images){
+    //     console.log(images)
+    // })
+}
+
+
+
+function renderImgaContainer(){
+    imageContainer.innerHTML = ''
+    for (const container of state.images){
+    const articleEl = document.createElement('article')
+    articleEl.setAttribute('class', 'image-card')
+
+    const h2TextEl = document.createElement('h2')
+    h2TextEl.textContent = container.title
+
+    const imageEl = document.createElement('img')
+    imageEl.setAttribute('class', 'image')
+    imageEl.setAttribute('src', container.image)
+
+    const likesDivEl = document.createElement('div')
+    likesDivEl.setAttribute ('class', 'likes-section')
+
+    const spanEL = document.createElement('span')
+    spanEL.setAttribute('class', 'likes')
+    spanEL.textContent = container.likes
+
+    const LikeBtnEl = document.createElement('button')
+    LikeBtnEl.setAttribute('class', 'like-button')
+    LikeBtnEl.textContent = '♥'
+
+    const comentUlEl = document.createElement('ul')
+    comentUlEl.setAttribute('class', 'comments')
+    for (const comment of container.comments){
+        const comentLiEl = document.createElement('li')
+        comentLiEl.textContent = comment.content
+        comentUlEl.append(comentLiEl)
+    }
+
+    const commentFormSection = document.createElement('form')
+    commentFormSection.setAttribute('class', 'comment-form')
+
+    const comentInputEl = document.createElement('input')
+    comentInputEl.setAttribute('class', 'comment-input')
+    comentInputEl.setAttribute('type', 'text')
+    comentInputEl.setAttribute('name', 'comment')
+    comentInputEl.setAttribute('placeholder', 'Add a comment')
+
+    const cometBtnPost = document.createElement('button')
+    cometBtnPost.setAttribute('class','comment-button')
+    cometBtnPost.setAttribute('type','submit')
+    cometBtnPost.textContent = 'Post'
+          
+    likesDivEl.append(spanEL, LikeBtnEl)
+    commentFormSection.append(comentInputEl, cometBtnPost)
+    articleEl.append(h2TextEl, imageEl, likesDivEl, comentUlEl, commentFormSection)
+    imageContainer.append(articleEl)
+
+
+}
+}
+
+
+function render(){
+    renderImgaContainer()
+}
+
+getImagesFromServer().then(function(imageDataFromServer){
+    state.images = imageDataFromServer
+    render()
+})
+
+render()
